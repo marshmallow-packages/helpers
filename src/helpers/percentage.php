1ad52b75cba652e9e572f73e25a7a23910ead3bc
Stef van Esch <stef@marshmallow.dev>
2020-04-15 10:48:02 +0200

    added a percentage helper

---
+++ src/helpers/percentage.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!function_exists('percentage')) {
+
+	function percentage ($mark, $total)
+	{
+		if (is_array($mark) || $mark instanceof Countable) {
+		    $mark = count($mark);
+		}
+
+		if (is_array($total) || $total instanceof Countable) {
+		    $total = count($total);
+		}
+
+		if ($mark == 0 || $total == 0) {
+			return 0;
+		}
+
+		return ($mark / $total) * 100;
+	}
+}
