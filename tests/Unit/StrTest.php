7f37ca1716bd8ffe62561259531157619722648e
Stef van Esch <stef@marshmallow.dev>
2021-03-24 16:58:48 +0100

    Fixed Str::remove error

--- tests/Unit/StrTest.php
+++ tests/Unit/StrTest.php
@@ -2,27 +2,27 @@
 
 namespace Marshmallow\HelperFunctions\Tests\Unit;
 
-use Marshmallow\HelperFunctions\Tests\TestCase;
 use Marshmallow\HelperFunctions\Facades\Str;
+use Marshmallow\HelperFunctions\Tests\TestCase;
 
 class StrTest extends TestCase
 {
     /** @test */
     public function it_can_clean_a_phone_number()
     {
-    	$this->assertEquals(Str::cleanPhoneNumber('+316 28 998-954'), '31628998954');
+        $this->assertEquals(Str::cleanPhoneNumber('+316 28 998-954'), '31628998954');
     }
 
     /** @test */
     public function it_can_return_a_numbers_only_string()
     {
-    	$this->assertEquals(Str::numbersOnly('12 ABC 34'), '1234');
+        $this->assertEquals(Str::numbersOnly('12 ABC 34'), '1234');
     }
 
     /** @test */
     public function it_can_return_only_numbers_and_letters()
     {
-    	$this->assertEquals(Str::numbersAndLettersOnly('12-ABC ### 34'), '12ABC34');
+        $this->assertEquals(Str::numbersAndLettersOnly('12-ABC ### 34'), '12ABC34');
     }
 
     /** @test */
@@ -63,6 +63,15 @@ class StrTest extends TestCase
         $this->assertEquals(
             Str::phonenumberWithCountryCode('+31628998954', '31', false),
             '0031628998954'
+        );
+    }
+
+    /** @test */
+    public function it_can_remove_a_part_of_a_string()
+    {
+        $this->assertEquals(
+            Str::remove('Marshmallow.dev', '.dev'),
+            'Marshmallow'
         );
     }
 }
